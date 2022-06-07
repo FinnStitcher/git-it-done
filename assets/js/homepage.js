0f86842094c6038996dd2069bc93f81ffa8d501f
Finn Stitcher <foundfootage@protonmail.com>
2022-06-06 18:16:37 -0700

    data displays to page

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,21 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username.")
+    };
+};
+
 var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
@@ -9,9 +27,38 @@ var getUserRepos = function(user) {
         response.json()
         // do stuff with that promise
         .then(function (data) {
-            console.log(data)
+            displayRepos(data, user);
         });
     });
 };
 
-getUserRepos("finnstitcher");
+var displayRepos = function(repos, searchTerm) {
+    // Clear old content
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+
+    for (var i = 0; i < repos.length; i++) {
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML = `<i class='fas fa-times status-icon icon-danger'></i> ${repos[i].open_issues_count} issue(s)`;
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>"
+        };
+
+        repoEl.appendChild(titleEl);
+        repoEl.appendChild(statusEl);
+        repoContainerEl.appendChild(repoEl);
+    }
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
