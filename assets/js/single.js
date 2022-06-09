ae574ed4c9c07136b4388d8ff270fdcb55a13608
Finn Stitcher <foundfootage@protonmail.com>
2022-06-08 22:31:06 -0700

    warning if not all issues can be displayed

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -8,6 +9,10 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(data => {
                 displayIssues(data);
+
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                };
             });
         }
         else {
@@ -46,4 +51,15 @@ var displayIssues = function(issues) {
     };
 };
 
-getRepoIssues("finnstitcher/git-it-done");
+var displayWarning = function(repo) {
+    limitWarningEl.textContent = "This repo has more than 30 open issues. ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    limitWarningEl.appendChild(linkEl);
+};
+
+getRepoIssues("angular/angular");
